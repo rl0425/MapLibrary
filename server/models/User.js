aae6f8287af9f42039584dcca9198f8c808bed45
Park gi chan <60688877+rl0425@users.noreply.github.com>
2021-05-21 18:08:31 +0900

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -57,7 +57,6 @@ userSchema.pre('save', function (next) {
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
 
-    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
@@ -66,13 +65,8 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
     var user = this;
-    // console.log('user._id', user._id)
 
-    // jsonwebtoken을 이용해서 token을 생성하기 
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
-    // user._id + 'secretToken' = token 
-    // -> 
-    // 'secretToken' -> user._id
 
     user.token = token
     user.save(function (err, user) {
@@ -86,8 +80,6 @@ userSchema.statics.findByToken = function(token, cb) {
     // user._id + ''  = token
     //토큰을 decode 한다. 
     jwt.verify(token, 'secretToken', function (err, decoded) {
-        //유저 아이디를 이용해서 유저를 찾은 다음에 
-        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
         user.findOne({ "_id": decoded, "token": token }, function (err, user) {
             if (err) return cb(err);
             cb(null, user)
