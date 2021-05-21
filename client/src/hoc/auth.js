4214e4b5006c5d71cbf4ae4856bfa7e2fb0e5f4c
Park gi chan <60688877+rl0425@users.noreply.github.com>
2021-05-21 18:10:36 +0900

    Update auth.js

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -5,9 +5,6 @@ import { auth } from '../_actions/user_action';
 
 export default function (SpecificComponent, option, adminRoute = null) {
 
-    //null    =>  아무나 출입이 가능한 페이지
-    //true    =>  로그인한 유저만 출입이 가능한 페이지
-    //false   =>  로그인한 유저는 출입 불가능한 페이지
     function AuthenticationCheck(props) {
         const dispatch = useDispatch();
 
