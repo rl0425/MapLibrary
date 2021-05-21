c5c996ff576790e21018219d9de2e64a2367ed0c
Park gi chan <60688877+rl0425@users.noreply.github.com>
2021-05-21 18:07:46 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
-const cors = require('cors');
-//application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-//application/json 
-app.use(bodyParser.json());
-app.use(cookieParser());
 
-
-//app.use('/api/users', require('./routes/users'));
 app.use('/api/favorite', require('./routes/favorite'));
-
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -31,8 +20,6 @@ app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
 app.post('/api/users/register', (req, res) => {
 
-  //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
-  //그것들을  데이터 베이스에 넣어준다. 
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
@@ -45,10 +32,7 @@ app.post('/api/users/register', (req, res) => {
 
 app.post('/api/users/login', (req, res) => {
 
-  // console.log('ping')
-  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
-
     // console.log('user', user)
     if (!user) {
       return res.json({
@@ -57,20 +41,11 @@ app.post('/api/users/login', (req, res) => {
       })
     }
 
-    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
     user.comparePassword(req.body.password, (err, isMatch) => {
-      // console.log('err',err)
-
-      // console.log('isMatch',isMatch)
-
       if (!isMatch)
         return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-
-      //비밀번호 까지 맞다면 토큰을 생성하기.
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
-
-        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
         res.cookie("x_auth", user.token)
           .status(200)
           .json({ loginSuccess: true, userId: user._id })
@@ -79,11 +54,7 @@ app.post('/api/users/login', (req, res) => {
   })
 })
 
-
-// role 1 어드민    role 2 특정 부서 어드민 
-// role 0 -> 일반유저   role 0이 아니면  관리자 
 app.get('/api/users/auth', auth, (req, res) => {
-  //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -95,9 +66,8 @@ app.get('/api/users/auth', auth, (req, res) => {
     image: req.user.image
   })
 })
-app.use(cors());
+
 app.get('/api/users/logout', auth, (req, res) => {
-  // console.log('req.user', req.user)
   User.findOneAndUpdate({ _id: req.user._id },
     { token: "" }
     , (err, user) => {
@@ -108,10 +78,6 @@ app.get('/api/users/logout', auth, (req, res) => {
     })
 })
 
-
-
 app.use(cors());
-
 const port = 5000
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
